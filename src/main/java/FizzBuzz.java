cc400dd2526b79746e92fb753cddb1b7d63616cf
Madhumitha <madhusedulous@gmail.com>
2021-08-19 06:19:07 -0700

    Requirement 1 : Checking a number is Fizz or Buzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,22 @@
 public class FizzBuzz {
+    int num;
+
+    FizzBuzz(int num)
+    {
+        this.num = num;
+    }
+
+    public String fizzCheck() {
+        if(num%3==0)
+            return "Fizz";
+        else
+            return "No it is not a Fizz number";
+    }
+
+    public String buzzCheck() {
+        if(num%5==0)
+            return "Buzz";
+        else
+            return "No it is not a Buzz number";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,49 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Nested
+    public class FizzBuzzCheck1
+    {
+        @Test
+        void toFindAMultipleOfThreeNumberIsFizz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(9);
+            String expectednumber = "Fizz";
+            String calculatednumber = fizzbuzznumber.fizzCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANonMultipleOfThreeNumberIsFizz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(26);
+            String expectednumber = "No it is not a Fizz number";
+            String calculatednumber = fizzbuzznumber.fizzCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindAMultipleOfFiveNumberIsBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(95);
+            String expectednumber = "Buzz";
+            String calculatednumber = fizzbuzznumber.buzzCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANonMultipleOfFiveNumberIsBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(59);
+            String expectednumber = "No it is not a Buzz number";
+            String calculatednumber = fizzbuzznumber.buzzCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+    }
+
 }
