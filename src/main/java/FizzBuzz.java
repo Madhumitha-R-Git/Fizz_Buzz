ed14d6d0130039b311026c2bb4aa8b019867edfd
Madhumitha <madhusedulous@gmail.com>
2021-08-19 20:29:38 +0530

    Requirement 2 : Checking a number is Fizz or Buzz or FizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -19,4 +19,40 @@ public class FizzBuzz {
         else
             return "No it is not a Buzz number";
     }
+
+    public String fizzAnotherCheck() {
+        if(num%3==0)
+            return "Fizz";
+        else  {
+            while(num>0)
+            {
+                if(num%10==3)
+                    return"Fizz";
+                num=num/10;
+            }
+
+        }
+        return "It is not a Fizz number";
+    }
+
+    public String buzzAnotherCheck() {
+        if(num%5==0)
+            return "Buzz";
+        else  {
+            while(num>0)
+            {
+                if(num%10==5)
+                    return"Buzz";
+                num=num/10;
+            }
+
+        }
+        return "It is not a Buzz number";
+    }
+
+    public String fizzbuzzCheck() {
+        if(num%3==0 && num%5==0)
+            return "FizzBuzz";
+        return "It is not a FizzBuzz";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -46,4 +46,97 @@ public class FizzBuzzTest {
 
     }
 
+    @Nested
+    public class FizzBuzzCheck2
+    {
+        @Test
+        void toFindAMultipleOfThreeNumberHavingThreeInItIsFizz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(33);
+            String expectednumber = "Fizz";
+            String calculatednumber = fizzbuzznumber.fizzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANonMultipleNumberOfThreeDoesNotHavingThreeInItIsFizz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(44);
+            String expectednumber = "It is not a Fizz number";
+            String calculatednumber = fizzbuzznumber.fizzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindAMultipleOfThreeNumberDoesNotHavingThreeInItIsFizz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(27);
+            String expectednumber = "Fizz";
+            String calculatednumber = fizzbuzznumber.fizzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANonMultipleOfThreeNumberHavingThreeInItIsFizz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(53);
+            String expectednumber = "Fizz";
+            String calculatednumber = fizzbuzznumber.fizzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindAMultipleOfFiveNumberHavingFiveInItIsBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(45);
+            String expectednumber = "Buzz";
+            String calculatednumber = fizzbuzznumber.buzzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANonMultipleNumberOfFiveDoesNotHavingFiveInItIsBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(44);
+            String expectednumber = "It is not a Buzz number";
+            String calculatednumber = fizzbuzznumber.buzzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindAMultipleOfFiveNumberDoesNotHavingFiveInItIsBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(20);
+            String expectednumber = "Buzz";
+            String calculatednumber = fizzbuzznumber.buzzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANonMultipleOfFiveNumberHavingFiveInItIsBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(54);
+            String expectednumber = "Buzz";
+            String calculatednumber = fizzbuzznumber.buzzAnotherCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANumberIsFizzBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(60);
+            String expectednumber = "FizzBuzz";
+            String calculatednumber = fizzbuzznumber.fizzbuzzCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+
+        @Test
+        void toFindANumberIsNotAFizzBuzz()
+        {
+            FizzBuzz fizzbuzznumber = new FizzBuzz(58);
+            String expectednumber = "It is not a FizzBuzz";
+            String calculatednumber = fizzbuzznumber.fizzbuzzCheck();
+            assertEquals(expectednumber,calculatednumber);
+        }
+    }
 }
